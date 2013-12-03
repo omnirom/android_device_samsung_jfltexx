fcbe1ce2022a43dd599c84c4ff43d61f5402f0e8
maxwen <max.weninger@gmail.com>
2013-12-03 02:04:06 +0100

    jfltexx: added gsm.mk for apn config
    Change-Id: I146e9df6696475cccdd04033ed4fca751ecee112

--- omni_jfltexx.mk
+++ omni_jfltexx.mk
@@ -13,6 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Get the prebuilt list of APNs
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
