2c5af2649e76b7f604c027d0a36c768b8efebf9f
Jake Whatley <jacob.whatley91@gmail.com>
2013-12-18 06:20:29 -0500

    jfltexx: cleanup
    remove non-existent overlays
    
    Signed-off-by: Jake Whatley <jacob.whatley91@gmail.com>
    Change-Id: I78069c72267b2948bf07761b3155d9800f82c87f

--- device.mk
+++ device.mk
@@ -17,8 +17,5 @@
 ## (2) Also get non-open-source specific aspects if available
 $(call inherit-product-if-exists, vendor/samsung/jfltexx/jfltexx-vendor.mk)
 
-## common overlays
-DEVICE_PACKAGE_OVERLAYS += device/samsung/jf-common/overlay-gsm
-
 # Inherit from jf-common
 $(call inherit-product, device/samsung/jf-common/jf-common.mk)
